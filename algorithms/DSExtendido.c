b927b651f4d8c26a8434c581ec304d56b9a3100d
Caio Rocha <cw_rocha@hotmail.com>
2018-06-19 07:30:43 -0300

    adding new perspective to generate a spanning tree

--- algorithms/DSExtendido.c
+++ algorithms/DSExtendido.c
@@ -18,3 +18,27 @@ int DSExtendido(v)
 
   return ConjuntoDS
 }
+
+//L[i] é um DS
+//T é um conjunto de DSs
+int DSEstendido(gr G, ori v, T){
+  for v in G
+    v.add = 0
+
+  L = vazio
+
+  for v in G
+    for u in adj[v]
+      if u not in L
+        u.add = 1
+        L += u
+        v = u
+      else if u.add in L == 1
+        u.add = 2
+        L[i] += u
+        v = u
+
+  L[i] is not in T
+    T += L[i]
+    DSEstendido(gr G, ori v, T)
+}
